b59beff20ee55f0b8a95a755d846baa2a95278c6
Jason Lombardozzi <jason.lombardozzi@gmail.com>
2012-09-03 20:50:36 -0400

    Add xml and app_class_convert

--- admin/applications_addon/ips/convert/app_class_convert.php
+++ admin/applications_addon/ips/convert/app_class_convert.php
@@ -18,5 +18,8 @@ class app_class_convert
 	function __construct( ipsRegistry $registry )
 	{
 		require_once( IPS_ROOT_PATH . 'applications_addon/ips/convert/sources/interface_acp.php' );
+		
+		/* Where are we? http://community.invisionpower.com/tracker/issue-37156-adding-tags-and-conversions/ */
+		define( 'IN_CONVERTER', true );		
 	}
 }
